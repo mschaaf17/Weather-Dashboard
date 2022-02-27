59d957899fa50206b779711e13de1c2924f5450b
Maddy <madisonschaaf48@gmail.com>
2022-02-27 11:39:37 -0700

    daily forecast changes with each search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,28 @@
-//?q=London this is a key and then value to add api key _=_&_=_&
-//var requestUrl = fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=e999a470cbbde7da1e47653e4e675d9d&units=metric');
 
-//setting up the url
+//setting up the url for current day
 var submitBtn = document.getElementById('submit');
 var api = 'https://api.openweathermap.org/data/2.5/weather?q=';
 var apiKey = '&appid=e999a470cbbde7da1e47653e4e675d9d'; 
-var units = '&units=metric';
-var invalid = document.querySelector('.msg');
+var units = '&units=imperial';
+var daily = '&daily';
+//var invalid = document.querySelector('.msg');
+//https://api.openweathermap.org/data/2.5/onecall?lat=40.6669&lon=-111.888&exclude=hourly,minutely&appid=e999a470cbbde7da1e47653e4e675d9d
 
-//set up local storage
+//make button to update url with value
+submitBtn.addEventListener("click", function(event) {
+  var inputEl = document.querySelector('#city-name').value.trim();
+  event.preventDefault(); 
+  var currentUrl = api + inputEl + daily + apiKey + units;
+  console.log(currentUrl)
+
+
+//everytime its clicked it repeats the local storage!
+  //set up local storage
 var citiesSearched = !!localStorage.getItem('citiesSearched') ? JSON.parse(localStorage.getItem('citiesSearched')) : [];
 citiesSearched.push(inputEl);
 localStorage.setItem('citiesSearched', JSON.stringify(citiesSearched));
 
-//display local storage 
+//display local storage ---stay after refresh
 for(i=0; i<citiesSearched.length; i++) {
   var cityListContainer = document.querySelector('#cityListContainer');
   var inputEl = document.querySelector('#city-name').value.trim();
@@ -24,71 +33,67 @@ for(i=0; i<citiesSearched.length; i++) {
   //once clicked the submitBtn function has to run again so this can't be in it!
   cityList.addEventListener('click', function(e) {
     console.log('clicked')
+    if('clicked') {
+      //find a way to make the click to match the value
+      return displayCurrentWeather();
+    }
   }) 
   }
 
 //if the local storage is in the submitBtn it works
 
+//function set up to retrieve information on a repeat click
+var displayCurrentWeather = function() {
 
-//make button to update url with value
-submitBtn.addEventListener("click", function(event) {
-  var inputEl = document.querySelector('#city-name').value.trim();
-  event.preventDefault(); 
-  var updatedUrl = api + inputEl + apiKey + units;
-  console.log(updatedUrl)
 
- 
-
-  
-
-//display name, date, temp, wind, humidity, wind
-var currentWeather = document.getElementById('current-day-container')
-var currentName= document.createElement('h2')
-currentName.textContent = inputEl;
-
-var currentDate = document.createElement('h2')
-var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()
-currentDate.textContent = date
-
-  //fetch data to display on the page
-  fetch(updatedUrl) 
+//fetch data to display on the page
+  fetch(currentUrl) 
   .then(function(response) {
     if(response.ok) {
     return response.json()
     .then(function(data) {
       console.log(data)
+      //container for all info
+      var currentWeather = document.getElementById('current-day-container')
+      //name data
+      var cityName = document.getElementById('name')
+      cityName.textContent = data.name
+      currentWeather.appendChild(cityName)
+
+      //date data
+      var currentDate = document.getElementById('date')
+      var today = new Date();
+      var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()
+      currentDate.textContent = date
+      currentWeather.appendChild(currentDate)
+
       //daily temp data
-        var dailyTemp = document.createElement('p')
-        dailyTemp.textContent = 'Temp: ' + Math.floor((data.main.temp*1.8)+32) + String.fromCharCode(176) + ' F'
+        var dailyTemp = document.getElementById('dailyTemp')
+        dailyTemp.textContent = 'Temp: ' + Math.round(data.main.temp) + String.fromCharCode(176) + ' F'
         currentWeather.appendChild(dailyTemp)
       //daily wind data
-        var dailyWind = document.createElement('p')
+        var dailyWind = document.getElementById('wind')
         dailyWind.textContent = 'Wind: ' + data.wind.speed + ' MPH'
         currentWeather.appendChild(dailyWind)
       //daily humidity
-      var dailyHumidity= document.createElement('p')
+      var dailyHumidity= document.getElementById('humidity')
       dailyHumidity.textContent = 'Humidity: ' + data.main.humidity + '%'
       currentWeather.appendChild(dailyHumidity)
 
-      
-      
+      //weather icon
+      //var icon = document.createElement('img')
+      var iconEl= document.getElementById('icon')
+      icon= data.weather[0].icon
+      iconEl.src= 'http://openweathermap.org/img/wn/' + icon + '@2x.png'
+      iconEl.appendChild(icon)
+    
     })
   }
   })
+  
+}
 
-
-
-currentWeather.appendChild(currentName)
-currentWeather.appendChild(currentDate)
-
-
-
-
-
-
-
-
+displayCurrentWeather();
 
 
 })
